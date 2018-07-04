7474efd58bbd42353f712dda877fc8fd8b1f17d3
NamJK <namjkyou@naver.com>
2018-07-04 11:54:20 +0900

    java day01
    기본형 boolean, char 까지

---
+++ day01-Hello/src/hello/Hello.java
@@ -0,0 +1,26 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 자바 기본 출력 구문
+		// sysout -> ctrl + spacebar
+		// code assistant에서 선택
+		System.out.println("Hellow, World!");
+		
+		// 실행 ctrl + F11
+		
+/*	 ______
+	/ㅇ<>ㅇ\
+   /|귀여워|\   
+    |______|
+     \    /   */
+
+	/*	 __!!__
+		/ㅇ<>ㅇ\
+	   /|귀여워|\   
+	    |______|
+	     \    /   */
+	}
+
+}
